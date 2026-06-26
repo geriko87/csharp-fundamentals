d711da511bae48e7281fb43e617814b38e57c802
DELL <DELL@DESKTOP-VQTTCJQ>
2026-06-26 15:58:40 +0200

    Add project files.

---
+++ 01_Basics/01_HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 01 - Hello World
+            Console.WriteLine("Hello, World!");
+
+            // Keep console open until Enter is pressed
+            Console.ReadLine();
+        }
+    }
+}

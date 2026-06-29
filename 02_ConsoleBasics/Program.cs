f9c9b688e21fa71bd84b0489ef417d49e1f7e420
DELL <DELL@DESKTOP-VQTTCJQ>
2026-06-29 23:13:58 +0200

    Add Console basics exercises with window settings and colors

---
+++ 02_ConsoleBasics/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _02_ConsoleBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 02 - Console Basics
+            
+            Console.WindowHeight = 25;
+            Console.WindowWidth = 100;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.Clear();
+
+            Console.SetCursorPosition(10, 5);
+            Console.CursorVisible = false; // Keep the cursor hidden for a cleaner console output.
+
+            Console.WriteLine("Hello, World!");
+
+            // Keep console open until Enter is pressed
+            Console.ReadLine();
+
+        }
+    }
+}

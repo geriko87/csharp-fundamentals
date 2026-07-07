1acf83009bf5c03c007fa32c71e1fa5400c1cb00
DELL <DELL@DESKTOP-VQTTCJQ>
2026-07-07 20:50:41 +0200

    Add 04_AgeCalculator project implementation

---
+++ 01_Basics/04_AgeCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _04_AgeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your year of birth: ");
+            int yearOfBirth = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int currentYear = DateTime.Now.Year;
+            int currentAge = currentYear - yearOfBirth;
+            Console.WriteLine($"You are {currentAge} years old in {currentYear}.");
+            Console.WriteLine($"You will be {currentAge + 1} next year.");
+        }
+    }
+}

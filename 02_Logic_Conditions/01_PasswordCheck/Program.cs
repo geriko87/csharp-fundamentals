1ab2d166a33fa97be03142abd4cd20077056f388
DELL <DELL@DESKTOP-VQTTCJQ>
2026-07-14 19:58:13 +0200

    Add 01_PasswordCheck project implementation

---
+++ 02_Logic_Conditions/01_PasswordCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01_PasswordCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the password: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Enter the password again: ");
+            string passwordAgain = Console.ReadLine();
+            Console.Clear();
+
+            if (password == passwordAgain)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Password accepted.");
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Incorrect password.");
+            }
+            Console.ResetColor();
+        }
+    }
+}

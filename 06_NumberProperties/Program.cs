bbcd856f5ac43733d80e6511c2c68917b1bf274b
DELL <DELL@DESKTOP-VQTTCJQ>
2026-07-23 20:20:31 +0200

    Add 06_NumberProperties implementation

---
+++ 06_NumberProperties/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06_NumberProperties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The {number} is even.");
+            }
+            else 
+            {
+                Console.WriteLine($"The {number} is odd.");
+            }
+            Console.WriteLine();
+
+            int counter = 0;
+            for (int i = 2; i < number; i++) 
+            {
+                if (number % i == 0)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine($"Positive divisors of the {number} is {counter}.");
+            Console.WriteLine();
+
+            if (number <= 1) 
+            {
+                Console.WriteLine($"The {number} is neither prime nor composite.");
+            }
+            else if (counter > 0)
+            {
+                Console.WriteLine($"The {number} is a composite number.");
+            }
+            else
+            {
+                Console.WriteLine($"The {number} is a prime number.");
+            }
+        }
+    }
+}

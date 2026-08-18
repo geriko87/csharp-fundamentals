46c3edcb17743ee99ee5f327c2b4c82f138001c4
DELL <DELL@DESKTOP-VQTTCJQ>
2026-08-18 22:49:18 +0200

    Add 09_ConsoleTimer implementation

--- 03_Loops_Random_Math/09_ConsoleTimer/Program.cs
+++ 03_Loops_Random_Math/09_ConsoleTimer/Program.cs
@@ -7,17 +7,17 @@
             Console.Write("Specify a time in seconds: ");
             int timeInSeconds = int.Parse(Console.ReadLine());
 
-            for (int i = timeInSeconds; i >= 0; i--) 
+            for (int i = timeInSeconds; i >= 0; i--)
             {
                 Console.Clear();
 
                 int inMinutes = i / 60;
                 int remainderSeconds = i % 60;
 
-                if (i < 10) 
+                if (i < 10)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Beep(); 
+                    Console.Beep();
                 }
                 Console.WriteLine($"{inMinutes:D2}:{remainderSeconds:D2}");
                 System.Threading.Thread.Sleep(1000);
